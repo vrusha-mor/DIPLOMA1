3682c6aefea0a20b11ef805cf65231c6cf4ae04d
vrusha-morakhiya <vrusha.morakhiya@senkusha.in>
2025-04-30 15:14:48 +0530

    Fix Python path to locate shared module

--- mind_verse_api/main_voice_app.py
+++ mind_verse_api/main_voice_app.py
@@ -1,5 +1,9 @@
-from flask import Flask, request, jsonify, send_file
+import sys
 import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from flask import Flask, request, jsonify, send_file
 import speech_recognition as sr
 import google.generativeai as genai
 import datetime
@@ -10,10 +14,7 @@ from mind_verse_api.shared.constants import GEMINI_API_KEY, ELEVEN_LABS_API_KEY
 from flask_cors import CORS  # Import the CORS package
 from .elevenlabs_api import TextToSpeechService
 
-import sys
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
 warnings.simplefilter('ignore')
